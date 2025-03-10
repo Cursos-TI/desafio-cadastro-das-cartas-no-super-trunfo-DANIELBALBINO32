66298bdd3732c7728a1e92f8a2afd682cdcd9f64
DANIELBALBINO32 <danieljuniormgdjsl@gmail.com>
2025-03-10 14:31:05 +0000

    trabalho pronto

--- .vscode/supertrunfo.c
+++ .vscode/supertrunfo.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 
-int main(){
-    printf("Desafio super trunfo\n");
+int main() {
+    char inicial = "G";
+    char código[20] = "G01";
+    char nome[20] = "GOIÁS";
+    int população = 6315900;
+    float área = 15244.19;
+    float PIB = 765.233;
+    int turísticos = 43;
 
-}
+    printf("inicial: %s\n", inicial)
+    printf("código: %s\n", código)
+    printf("nome: %s\n", nome)
+    printf("população: %d\n", população)
+    printf("área: %.2f KM²\n", área)
+    printf("PIB: %.2f Bilhões\n", PIB)
+    printf("turísticos: %d\n", turísticos)
+
+return 0;
+
+
+
+
+
+
+
 
